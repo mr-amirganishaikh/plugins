e3382eae2112eb9e2256ec691855483f2956ea0a
ShaikhAmir <shaikhamir954@gmail.com>
2017-11-19 16:56:02 +0530

    update two

--- customInput/customCheck.js
+++ customInput/customCheck.js
@@ -6,8 +6,7 @@
         var localOptions = {
             handle: '',
 
-            checkboxTheme: 'customCheck',
-            radioTheme: 'customRadio',
+            customTheme: '',
 
             checkedClass: 'checked',
             checkedCheckboxClass: '',
@@ -25,80 +24,51 @@
             enabledCheckboxClass: '',
             enabledRadioClass: '',
 
-            hover: false,
+            hover: true,
             hoverClass: 'hover',
 
-            labelHover: false,
+            labelHover: true,
             labelHoverClass: 'hover'
         };
 
         // merge default and user options
         options = $.extend(localOptions, options);
-        console.log(options);
-        
+
         // traverse all nodes
         this.each(function () {
-            // To check default status of input
-            var checkStatusTheme = function (checkedStatus, disabledStatus) {
-                if (checkedStatus === true && disabledStatus === false) {
-                    return ' checked';
-                } else if (checkedStatus === false && disabledStatus === true) {
-                    return ' disabled';
-                } else if (checkedStatus === true && disabledStatus === true) {
-                    return ' checked disabled';
-                }
-                return '';
-            }
-            
-            // To check default status of input
-            var checkInputTheme = function (targetType) {
-                /*console.log(targetType);*/
-                if (targetType !== undefined && targetType === 'checkbox') {
-                    return options.checkboxTheme;
-                } else if (targetType !== undefined && targetType === 'radio') {
-                    return options.radioTheme;
-                }
-            }
-
             // Variables declaration
             var $this = $(this),
                 checkedStatus = $this.prop('checked'),
                 disabledStatus = $this.prop('disabled'),
-                theme = checkInputTheme($this.attr('type')),
-                $theme = '.' + theme,
-                statusTheme = checkStatusTheme(checkedStatus, disabledStatus),
-                parentBody = '<div class="' + theme + statusTheme + '" aria-checked="' + checkedStatus + '" aria-disabled="' + disabledStatus + '"></div>';
-
-            $($theme).off();
-            $($theme).siblings('label').off();
+                $customCheck = '.customCheck',
+                parentBody = '<div class="customCheck ' + options.customTheme + '" aria-checked="' + checkedStatus + '" aria-disabled="' + disabledStatus + '"></div>',
+                customInputHelper = '<ins class="customCheck_helper"></ins>';
             
+            // Set off for all events as default
+            $($customCheck).off();
+            $($customCheck).siblings('label').off();
+            
+            // Create the customCheck block
             $this.wrap(parentBody);
+            $(customInputHelper).insertAfter($this);
 
-            $this.css({
-                'position': 'absolute',
-                'top': '-20%',
-                'left': '-20%',
-                'display': 'block',
-                'width': '100%',
-                'height': '100%',
-                'margin': '0',
-                'padding': '0',
-                'opacity': '0',
-                'border': '0',
-                'z-index': '-1'
+            // set display as per customInput
+            $($customCheck).css({
+                'height': $(this).find('.customCheck_helper').outerHeight(),
+                'width': $(this).find('.customCheck_helper').outerWidth()
             });
-
+            
             // To check the status of Input
             var checkStatus = function (target) {
                 target.prop("checked", true);
-                target.parent($theme).attr('aria-checked', 'true').addClass('checked');
+                target.parent($customCheck).attr('aria-checked', 'true').addClass('checked');
                 return true;
             }
 
             // To uncheck the status of Input
             var uncheckStatus = function (target) {
                 target.prop("checked", false);
-                target.parent($theme).attr('aria-checked', 'false').removeClass('checked');
+                target.parent($customCheck).attr('aria-checked', 'false').removeClass('checked');
                 return true;
             }
 
@@ -116,24 +86,27 @@
                 return false;
             }
             
-            if (options.labelHover === true) {
-                $this.parent($theme).siblings('label').on('mouseover', function () {
-                    $(this).addClass(options.labelHoverClass).siblings($theme).addClass(options.hoverClass);
-                }).on('mouseout', function () {
-                    $(this).removeClass(options.labelHoverClass).siblings($theme).removeClass(options.hoverClass);
-                });
-            }
-
+            
             if (options.hover === true) {
-                $this.parent($theme).on('mouseover', function () {
-                    console.log(options.hover);
+                $($customCheck).on('mouseenter', function () {
                     $(this).addClass(options.hoverClass);
-                }).on('mouseout', function () {
+                }).on('mouseleave', function () {
                     $(this).removeClass(options.hoverClass);
                 });
             }
 
-            $($theme).on("click", function () {
+            if (options.labelHover === true) {
+                $($customCheck).siblings('label').on('mouseenter', function () {
+                    $(this).addClass(options.labelHoverClass);
+                    $(this).siblings($customCheck).addClass(options.hoverClass);
+                }).on('mouseleave', function () {
+                    $(this).removeClass(options.labelHoverClass);
+                    $(this).siblings($customCheck).removeClass(options.hoverClass);
+                });
+            }
+            
+            // main on click event
+            $($customCheck).on("click", function () {
                 toggleStatus($(this).children('input'), $(this).children('input').prop('checked'), $(this).children('input').prop('disabled'));
             });
 
